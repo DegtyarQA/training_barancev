e7cde4eb24971a416f841e7ee4e6cfd1f1390017
valery.degtyar <degtyar060589@gmail.com>
2022-12-24 22:01:09 +0300

    pytest nachalo

--- test_add_group.py
+++ test_add_group.py
@@ -1,32 +1,21 @@
-import unittest
+import pytest
 from application import Application
 from group import Group
 
-def is_alert_present(wd):
-    try:
-        wd.swich_to_alert().text
-        return True
-    except:
-        return False
 
+@pytest.fixture
+def app(request):
+    fixture = Application()
+    request.addfinalizer(fixture.destroy())
+    return fixture
 
-class test_add_group(unittest.TestCase):
-    def setUp(self):
-        self.app = Application()
+def test_add_group(app):
+    app.login(username = "admin", password = "secret")
+    app.create_group(Group(name='fff', header='rrrr', footer='refdfd'))
+    app.logout()
 
-    def test_add_group(self):
-        self.app.login(username = "admin", password = "secret")
-        self.app.create_group(Group(name='fff', header='rrrr', footer='refdfd'))
-        self.app.logout()
+def test_add_empty_group(app):
+    app.login(username='admin', password='secret')
+    app.create_group(Group(name='', header='', footer=''))
+    app.logout()
 
-    def test_add_empty_group(self):
-        self.app.login(username='admin', password='secret')
-        self.app.create_group(Group(name='', header='', footer=''))
-        self.app.logout()
-
-    def tearDown(self):
-        self.app.destroy()
-
-
-if __name__ == '__main__':
-    unittest.main()
