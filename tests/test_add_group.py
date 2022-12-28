cd001f626119358481ef959d759392198c00238c
valery.degtyar <degtyar060589@gmail.com>
2022-12-28 14:49:52 +0300

    Декомпозиция окончание

--- tests/test_add_group.py
+++ tests/test_add_group.py
@@ -11,13 +11,13 @@ def app():
 
 def test_add_group(app):
     app.session.login(username = "admin", password = "secret")
-    app.create_group(Group(name='fff', header='rrrr', footer='refdfd'))
+    app.group.create(Group(name='fff', header='rrrr', footer='refdfd'))
     app.session.logout()
     app.destroy()
 
 def test_add_empty_group(app):
     app.session.login(username='admin', password='secret')
-    app.create_group(Group(name='', header='', footer=''))
+    app.group.create(Group(name='', header='', footer=''))
     app.session.logout()
     app.destroy()
 
